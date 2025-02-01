19a8aa55998ba84b435bd76f3e62f5325ee8a18e
Piyush Gupta <piyushgupta1165@gmail.com>
2025-02-01 13:28:38 +0530

    first commit

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+    
+};
+
+export const login = (req, res) => {
+    res.send("login routes");
+};
+
+export const logout = (req, res) => {
+    res.send("logout routes");
+};
---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    console.log("server listening on port: " + PORT);
+});
